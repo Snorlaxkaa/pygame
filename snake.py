228e01927e395fb5bcf3391fc284dfcd20c074a1
Snorlaxkaa <johnny941114@gmail.com>
2023-12-20 18:35:44 +0800

    1

---
+++ snake.py
@@ -0,0 +1,232 @@
+import math
+import random
+import pygame
+import tkinter as tk
+from tkinter import messagebox
+
+class cube(object):
+    rows = 20
+    w = 500
+    def __init__(self,start,dirnx=1,dirny=0,color=(255,0,0)):
+        self.pos = start
+        self.dirnx = dirnx
+        self.dirny = dirny
+        self.color = color
+      
+    def move(self, dirnx, dirny):
+        self.dirnx = dirnx
+        self.dirny = dirny
+        self.pos = (self.pos[0] + self.dirnx, self.pos[1] + self.dirny)
+    
+    def draw(self, surface, eyes=False):
+        dis = self.w // self.rows
+        i = self.pos[0]
+        j = self.pos[1]
+        
+        pygame.draw.rect(surface, self.color, (i*dis+1, j*dis+1, dis-2, dis-2))
+        if eyes:
+            centre = dis // 2
+            radius = 3
+            circleMiddle = (i*dis+centre-radius, j*dis+8)
+            circleMiddle2 = (i*dis + dis - radius*2, j*dis+8)
+            pygame.draw.circle(surface, (0,0,0), circleMiddle, radius)
+            pygame.draw.circle(surface, (0,0,0), circleMiddle2, radius)
+        
+
+class snake(object):
+    body = []
+    turns = {}
+    
+    def __init__(self, color, pos):
+        self.color = color
+        self.head = cube(pos)
+        self.body.append(self.head)
+        self.dirnx = 0
+        self.dirny = 1
+        
+        # 初始化貪食蛇的身體部分，使其長度為11
+        for _ in range(10):
+            self.addCube()
+        
+    def move(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                
+            keys = pygame.key.get_pressed()
+            
+            for key in keys:
+                if keys[pygame.K_LEFT]:
+                    self.dirnx = -1
+                    self.dirny = 0
+                    self.turns[self.head.pos[:]] = [self.dirnx, self.dirny]
+                    
+                elif keys[pygame.K_RIGHT]:
+                    self.dirnx = 1
+                    self.dirny = 0
+                    self.turns[self.head.pos[:]] = [self.dirnx, self.dirny]
+                    
+                elif keys[pygame.K_UP]:
+                    self.dirnx = 0
+                    self.dirny = -1
+                    self.turns[self.head.pos[:]] = [self.dirnx, self.dirny]
+                    
+                elif keys[pygame.K_DOWN]:
+                    self.dirnx = 0
+                    self.dirny = 1
+                    self.turns[self.head.pos[:]] = [self.dirnx, self.dirny]
+                    
+        for i, c in enumerate(self.body):
+            p = c.pos[:]
+            if p in self.turns:
+                turn = self.turns[p]
+                c.move(turn[0], turn[1])
+                if i == len(self.body)-1:
+                    self.turns.pop(p)
+            else:
+                if c.dirnx == -1 and c.pos[0] <= 0: 
+                    print("Game Over 分數:", len(self.body))  # 在終端機上打印遊戲結束消息和分數
+                    pygame.quit()  # 當碰到左邊界時結束遊戲
+                elif c.dirnx == 1 and c.pos[0] >= c.rows-1:
+                    print("Game Over 分數:", len(self.body))  # 在終端機上打印遊戲結束消息和分數
+                    pygame.quit()  # 當碰到右邊界時結束遊戲
+                elif c.dirny == 1 and c.pos[1] >= c.rows-1:
+                    print("Game Over 分數:", len(self.body))  # 在終端機上打印遊戲結束消息和分數
+                    pygame.quit()  # 當碰到下邊界時結束遊戲
+                elif c.dirny == -1 and c.pos[1] <= 0: 
+                    print("Game Over 分數:", len(self.body))  # 在終端機上打印遊戲結束消息和分數
+                    pygame.quit()  # 當碰到上邊界時結束遊戲
+                else: 
+                    c.move(c.dirnx,c.dirny)
+
+
+        
+    def reset(self, pos):
+        self.head = cube(pos)
+        self.body = []
+        self.body.append(self.head)
+        self.turns = {}
+        self.dirnx = 0
+        self.dirny = 1
+
+    def addCube(self):
+        tail = self.body[-1]
+        dx, dy = tail.dirnx, tail.dirny
+
+        if dx == 1 and dy == 0:
+            self.body.append(cube((tail.pos[0]-1,tail.pos[1])))
+        elif dx == -1 and dy == 0:
+            self.body.append(cube((tail.pos[0]+1,tail.pos[1])))
+        elif dx == 0 and dy == 1:
+            self.body.append(cube((tail.pos[0],tail.pos[1]-1)))
+        elif dx == 0 and dy == -1:
+            self.body.append(cube((tail.pos[0],tail.pos[1]+1)))
+
+        self.body[-1].dirnx = dx
+        self.body[-1].dirny = dy
+
+    def draw(self, surface):
+        for i, c in enumerate(self.body):
+            if i == 0:
+                c.draw(surface, True)
+            else:
+                c.draw(surface)
+        
+
+def drawGrid(w, rows, surface):
+    sizeBtwn = w // rows
+    
+    x = 0
+    y = 0
+    for l in range(rows):
+        x = x + sizeBtwn
+        y = y + sizeBtwn
+        
+        pygame.draw.line(surface, (255,255,255), (x,0),(x,w))
+        pygame.draw.line(surface, (255,255,255), (0,y),(w,y))
+        
+
+def redrawWindow(surface):
+    global rows, width, s
+    surface.fill((0,0,0))
+    snack.draw(surface)
+    snack2.draw(surface)
+    s.draw(surface)
+    drawGrid(width,rows,surface)
+    pygame.display.update()
+
+
+
+    
+
+def randomSnack(rows, snake):
+    positions = snake.body
+
+    while True:
+        x = random.randrange(rows)
+        y = random.randrange(rows)
+        if len(list(filter(lambda z:z.pos == (x,y), positions))) > 0:
+            continue
+        else:
+            break
+        
+    return (x,y)
+
+def message_box(subject, content):
+    root = tk.Tk()
+    root.attributes("-topmost", True)
+    root.withdraw()
+    messagebox.showinfo(subject, content)
+    try:
+        root.destroy()
+    except:
+        pass
+
+
+def random_color():
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    return (r, g, b)
+
+def main():
+    global width, rows, s, snack ,snack2
+    width = 500  # Width of our screen
+    height = 500  # Height of our screen
+    rows = 20  # Amount of rows
+
+    win = pygame.display.set_mode((width, height))  # Creates our screen object
+
+    s = snake((255,0,0), (10,10))  # Creates a snake object
+    snack = cube(randomSnack(rows, s), color=(255,255,255))  # Creates a snack object
+    snack2 = cube(randomSnack(rows, s), color=(255,255,255))  # 第二個食物
+
+    clock = pygame.time.Clock() # creating a clock object
+
+    flag = True
+    # STARTING MAIN LOOP
+    while flag:
+        pygame.time.delay(10)  # This will delay the game so it doesn't run too quickly
+        clock.tick(10)  # Will ensure our game runs at 10 FPS
+        s.move()
+        if s.body[0].pos == snack.pos:
+            s.addCube()
+            randomColor = random_color()  # 生成隨機顏色
+            snack = cube(randomSnack(rows, s), color=randomColor)  # 使用隨機顏色創建新的食物
+
+        if s.body[0].pos == snack2.pos:
+            s.addCube()
+            randomColor = random_color()  # 生成隨機顏色
+            snack2 = cube(randomSnack(rows, s), color=randomColor)  # 使用隨機顏色創建新的食物
+
+        for x in range(len(s.body)):  # Check if snake collides with itself
+            if s.body[x].pos in list(map(lambda z:z.pos,s.body[x+1:])):
+                print('Score: ', len(s.body))
+                message_box('You Lost!', 'Play again...')
+                s.reset((10,10))
+                break
+
+        redrawWindow(win)
+    pass
+
+main()
